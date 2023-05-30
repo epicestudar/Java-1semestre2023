4695635b1b391b83cd10f4bda476c8651cdb3837
Vinícius <107704540+epicestudar@users.noreply.github.com>
2023-05-30 20:00:48 +0000

    exercícios poo

--- PooExercicio2/Agenda.java
+++ PooExercicio2/Agenda.java
@@ -1,5 +1,30 @@
 package PooExercicio2;
 
 public class Agenda {
+    // atributos
+    String nome;
+    String altura;
+    String idade;
+
+    // métodos
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getAltura() {
+        return altura;
+    }
+    public void setAltura(String altura) {
+        this.altura = altura;
+    }
+    public String getIdade() {
+        return idade;
+    }
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+
     
 }
--- PooExercicio2/App.java
+++ PooExercicio2/App.java
@@ -1,7 +1,18 @@
 package PooExercicio2;
 
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) {
-        
+        Agenda obj[] = new Agenda[10]; // vetor de 10 objetos
+
+        for (int i = 0; i < obj.length; i++) {
+            obj[i] = new Agenda(); // construindo o objeto
+
+            // preencher meu objeto
+            obj[i].setNome(JOptionPane.showInputDialog("Informe o nome: "));
+            obj[i].setAltura((JOptionPane.showInputDialog("Informe a altura: ")));
+            obj[i].setIdade((JOptionPane.showInputDialog("Informe o idade: ")));
+        }
     }
 }
