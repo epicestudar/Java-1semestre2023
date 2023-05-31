9fcf10d341e8df9853d0007d4e975b845760f0f4
Vinícius <107704540+epicestudar@users.noreply.github.com>
2023-05-31 19:54:20 +0000

    continuacao execicio 2 java

--- PooExercicio2/Agenda.java
+++ PooExercicio2/Agenda.java
@@ -1,30 +1,72 @@
 package PooExercicio2;
+import java.util.Scanner;
 
 public class Agenda {
+    Scanner sc = new Scanner(System.in);
     // atributos
     String nome;
-    String altura;
-    String idade;
+    float altura;
+    int idade;
 
     // métodos
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
-    public String getAltura() {
+
+    public float getAltura() {
         return altura;
     }
-    public void setAltura(String altura) {
+
+    public void setAltura(float altura) {
         this.altura = altura;
     }
-    public String getIdade() {
+
+    public int getIdade() {
         return idade;
     }
-    public void setIdade(String idade) {
+
+    public void setIdade(int idade) {
         this.idade = idade;
     }
 
-    
+    public void armazenaPessoa() {
+        int cont=0;
+
+        while(cont < 2) {
+            System.out.println("Informe o seu nome: ");
+            setNome(sc.next());
+            System.out.println("Informe a sua idade: ");
+            setIdade(sc.nextInt());
+            System.out.println("Informe a sua altura: ");
+            setAltura(sc.nextFloat());
+
+            cont++;
+        }
+    }
+
+   public void buscaPessoa(){
+    String buscaPessoa;
+    System.out.println("Informe o nome da pessoa que quer procurar: ");
+    buscaPessoa = sc.next();
+
+    for (int i = 0; i < 2; i++) {
+        if(getNome().equals(buscaPessoa)){
+            System.out.println("Esta pessoa está na posição: " + (i+1));
+        }
+    }
+   }
+
+   public void imprimeAgenda() {
+    System.out.println("Dados das pessoas das agendas: ");
+
+    for (int i = 0; i < 2; i++) {
+        System.out.println(getNome());
+        System.out.println(getIdade());
+        System.out.println(getAltura());
+    }
+   }
 }
--- PooExercicio2/App.java
+++ PooExercicio2/App.java
@@ -4,15 +4,9 @@ import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) {
-        Agenda obj[] = new Agenda[10]; // vetor de 10 objetos
-
-        for (int i = 0; i < obj.length; i++) {
-            obj[i] = new Agenda(); // construindo o objeto
-
-            // preencher meu objeto
-            obj[i].setNome(JOptionPane.showInputDialog("Informe o nome: "));
-            obj[i].setAltura((JOptionPane.showInputDialog("Informe a altura: ")));
-            obj[i].setIdade((JOptionPane.showInputDialog("Informe o idade: ")));
-        }
+       Agenda obj1 = new Agenda();
+       obj1.armazenaPessoa();
+       obj1.imprimeAgenda();
+       
     }
 }
